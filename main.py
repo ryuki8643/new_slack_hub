0457c224d33825eaa8d1df2d022c985e651bddb3
ryuki8643 <garyuki88me@gmail.com>
2022-08-05 23:28:59 +0900

    Edit: not to make pycache

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import sys
+
+sys.dont_write_bytecode = True
+
 from qiita import qiita
 
 
 def main():
-
-    qiita.get_rss_qiita()
+    qiita.read_rss_qiita()
 
 
 if __name__ == '__main__':
     main()
-
-
