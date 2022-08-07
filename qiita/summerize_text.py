4698a58e5280ff0b91efce84490a5c94988ce0d4
ryuki8643 <garyuki88me@gmail.com>
2022-08-07 16:06:06 +0900

    Add:function to summerize text

--- qiita/summerize_text.py
+++ qiita/summerize_text.py
@@ -1,1 +1,21 @@
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.mecab_tokenizer import MeCabTokenizer
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
 
+
+def fn_start_document_summarize(document):
+    auto_abstractor = AutoAbstractor()
+
+    auto_abstractor.tokenizable_doc = MeCabTokenizer()
+
+    auto_abstractor.delimiter_list = ["。", "\n"]
+
+    abstractable_doc = TopNRankAbstractor()
+
+    result_dict = auto_abstractor.summarize(document, abstractable_doc)
+
+    result_text = ""
+
+    for sentence in result_dict["summarize_result"]:
+        result_text += sentence
+    return result_text
