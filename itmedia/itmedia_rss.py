a8fd4145a76f33506f358eafd317d80fa737868a
ryuki8643 <garyuki88me@gmail.com>
2022-08-08 23:24:30 +0900

    Add:function to connect itmedia with slack

--- itmedia/itmedia_rss.py
+++ itmedia/itmedia_rss.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import datetime
@@ -13,18 +15,28 @@ def read_rss_it_media():
 def day_check():
     now_day = datetime.datetime.now().isoweekday()
     day_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    return set(day_of_week[now_day - 1], day_of_week[now_day])
+    return set([day_of_week[now_day - 1], day_of_week[now_day]])
 
 
-def read_rss_qiita_txt():
+def read_rss_it_media_txt():
     txt = read_rss_it_media()
 
     rss_items = []
     valid_dates = day_check()
     for item in txt.findAll('item'):
-        if item.pubDate[:3] in valid_dates:
-            rss_item = "*" + item.pubDatea.text[5:16] + " " + item.title.text + "*\n" + item.description.text + "\n" + \
-                       item.link.text
+        if item.pubdate.text[:3] in valid_dates:
+            if item.description:
+                rss_item = "*" + item.pubdate.text[5:16] + " " + \
+                           item.title.text + "*\n" + \
+                           item.description.text + "\n" + \
+                           re.findall(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", item.text)[0]
+            else:
+                rss_item = "*" + item.pubdate.text[5:16] + " " + \
+                           item.title.text + "*\n" + \
+                           item.link.text + \
+                           re.findall(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", item.text)[0]
             rss_items.append(rss_item)
+        else:
+            break
 
-    return "\n".join(reversed(rss_items))
+    return "\n\n".join(reversed(rss_items))
