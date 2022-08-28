5fe178e8c51cd65230a3ef9e4598099ff5560997
ryuki8643 <garyuki88me@gmail.com>
2022-08-28 21:52:46 +0900

    Add: falsk configulation

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ import os
 import sys
 
 sys.dont_write_bytecode = True
-
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_sdk import WebClient
 from qiita.throw_qiita_toslack import *
 from zenn.throw_zenn_toslack import *
 from itmedia.throw_itmedia_toslack import *
 from googledev.throw_googledev_toslack import *
 
+
 # アプリを起動します
+
+from flask import Flask, request
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+@flask_app.route("/")
+def hello():
+    return "Hello World!"
+
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 8000)))
+    flask_app.run(host="0.0.0.0", port=8000)
