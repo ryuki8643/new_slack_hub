a7321f52e434f79b277cf0dcf009cf770307e0e9
ryuki8643 <garyuki88me@gmail.com>
2023-04-01 01:38:27 +0900

    dotenv

--- slackbot_init/_init_.py
+++ slackbot_init/_init_.py
@@ -4,13 +4,13 @@ import discord
 
 from dotenv import load_dotenv
 
-# .envファイルの内容を読み込みます
+
 load_dotenv()
 
 
 discord_client = discord.Client(intents=discord.Intents.all())
 
-# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
