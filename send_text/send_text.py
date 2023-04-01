212e1e9998d15fedd0abc2aac97ee4af6ddb98b0
ryuki8643 <garyuki88me@gmail.com>
2023-04-02 00:48:05 +0900

    url to environment variables

--- send_text/send_text.py
+++ send_text/send_text.py
@@ -1,25 +1,37 @@
-import requests, json
+import requests, json,os
 
 from enum import Enum
 from time import sleep
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 # 列挙型の定義
 class Channels(Enum):
     DAYLY = "daily"
     TECHBLOGS = "techblogs"
     OTHERTECHBLOGS = "othertechblogs"
 
-discord_channels={Channels.DAYLY:"https://discordapp.com/api/webhooks/1091710346466709684/R8SMkKPmuRx9PqwGOPqSdKCJkiA7T5zZ-0Pl96QT-b-8dSZDVQTYtVzjyP_u7J9h36Ev",
-                  Channels.TECHBLOGS:"https://discordapp.com/api/webhooks/1091710531678781450/qr8_7aQ_s6k9rlclsIqGplcsXYCq0FjixnFlT4OgVDA_aqwO0-05WWIQlKOYdWFWu0Fz",
-                  Channels.OTHERTECHBLOGS:"https://discordapp.com/api/webhooks/1091710702399533076/ZtgNz-KGF8vhicwvRLbBYnzOwbc1EYfZnDO7DeuiDQAJ9XuSN_fsV7bJA_9_BnBoBDLZ"}
+discord_channels={Channels.DAYLY:os.environ.get("DISCORD_WEBHOOK_DAILY"),
+                  Channels.TECHBLOGS:os.environ.get("DISCORD_WEBHOOK_TECHBLOG"),
+                  Channels.OTHERTECHBLOGS:os.environ.get("DISCORD_WEBHOOK_OTHERTECHBLOG")}
 def send_discord_and_slack(text,say,channel):
     say(text)
     webhook_url  = discord_channels[channel]
     split_text = text.split("\n")
-    for i in split_text:
+    for i in range(len(split_text)):
+        send_text=split_text[i]
+        if i+1<len(split_text):
+            if split_text[i+1][:4]=="http":
+                send_text+=split_text[i+1]
+                i+=1
+
+                
         main_content = {
                         'username': 'tech_news_bot',
                         'avatar_url': 'https://drive.google.com/file/d/1tdtv3bGehSVaf7ER0_SJqeIudeDbEwNY/view?usp=share_link',
-                        'content': i
+                        'content': send_text
                     }
         headers      = {'Content-Type': 'application/json'}
 
