333f4356b16adb940c85961e740186b70b5c2db3
ryuki8643 <garyuki88me@gmail.com>
2023-04-01 23:57:59 +0900

    remain

--- send_text/send_text.py
+++ send_text/send_text.py
@@ -1,7 +1,7 @@
 import requests, json
 
 from enum import Enum
-
+from time import sleep
 # 列挙型の定義
 class Channels(Enum):
     DAYLY = "daily"
@@ -14,11 +14,14 @@ discord_channels={Channels.DAYLY:"https://discordapp.com/api/webhooks/1091710346
 def send_discord_and_slack(text,say,channel):
     say(text)
     webhook_url  = discord_channels[channel]
-    main_content = {
-                    'username': 'tech_news_bot',
-                    'avatar_url': 'https://drive.google.com/file/d/1tdtv3bGehSVaf7ER0_SJqeIudeDbEwNY/view?usp=share_link',
-                    'content': text
-                }
-    headers      = {'Content-Type': 'application/json'}
+    split_text = text.split("\n")
+    for i in split_text:
+        main_content = {
+                        'username': 'tech_news_bot',
+                        'avatar_url': 'https://drive.google.com/file/d/1tdtv3bGehSVaf7ER0_SJqeIudeDbEwNY/view?usp=share_link',
+                        'content': i
+                    }
+        headers      = {'Content-Type': 'application/json'}
 
-    requests.post(webhook_url, json.dumps(main_content), headers=headers)
+        requests.post(webhook_url, json.dumps(main_content), headers=headers)
+        sleep(1)
--- techblogs/thow_tech_blog_rss.py
+++ techblogs/thow_tech_blog_rss.py
@@ -1,31 +1,33 @@
 from slackbot_init._init_ import app
 from techblogs import techblog_rss
-
+from send_text.send_text import send_discord_and_slack,Channels
 
 @app.message("gihyo")
 def message_hello1(message, say):
 
-    say("*Gihyo*\n\n"+techblog_rss.get_rss_tech_blog("https://gihyo.jp/dev/feed/rss2"))
+    send_discord_and_slack("*Gihyo*\n\n"+techblog_rss.get_rss_tech_blog("https://gihyo.jp/dev/feed/rss2"),say,Channels.TECHBLOGS)
 
 @app.message("aws")
 def message_hello1(message, say):
 
-    say("*aws*\n\n"+techblog_rss.get_rss_tech_blog("https://aws.amazon.com/jp/blogs/news/feed/"))
+    send_discord_and_slack("*aws*\n\n"+techblog_rss.get_rss_tech_blog("https://aws.amazon.com/jp/blogs/news/feed/"),say,Channels.TECHBLOGS)
 
 @app.message("mercari")
 def message_hello1(message, say):
+    send_discord_and_slack("*Mercari*\n\n"+techblog_rss.get_rss_tech_blog("https://engineering.mercari.com/blog/feed.xml"),say,Channels.TECHBLOGS)
 
-    say("*Mercari*\n\n"+techblog_rss.get_rss_tech_blog("https://engineering.mercari.com/blog/feed.xml"))
 @app.message("pfn")
 def message_hello1(message, say):
-    say("*Preferred Network*\n\n"+techblog_rss.get_rss_tech_blog("https://tech.preferred.jp/ja/feed/"))
+    send_discord_and_slack("*Preferred Network*\n\n"+techblog_rss.get_rss_tech_blog("https://tech.preferred.jp/ja/feed/"),say,Channels.TECHBLOGS)
+
 @app.message("developperio")
 def message_hello1(message, say):
-    say("*DevelopperIO*\n\n"+techblog_rss.get_rss_tech_blog("https://dev.classmethod.jp/feed/"))
+    send_discord_and_slack("*DevelopperIO*\n\n"+techblog_rss.get_rss_tech_blog("https://dev.classmethod.jp/feed/"),say,Channels.TECHBLOGS)
+
 @app.message("itmedia")
 def message_hello1(message, say):
+    send_discord_and_slack("*ITmedia*\n\n"+techblog_rss.get_rss_tech_blog("https://rss.itmedia.co.jp/rss/2.0/ait.xml"),say,Channels.TECHBLOGS)
 
-    say("*ITmedia*\n\n"+techblog_rss.get_rss_tech_blog("https://rss.itmedia.co.jp/rss/2.0/ait.xml"))
 @app.message("yahoo")
 def message_hello1(message, say):
-    say("*Yahoo*\n\n"+techblog_rss.get_rss_tech_blog("https://techblog.yahoo.co.jp/index.xml"))
+    send_discord_and_slack("*Yahoo*\n\n"+techblog_rss.get_rss_tech_blog("https://techblog.yahoo.co.jp/index.xml"),say,Channels.TECHBLOGS)
